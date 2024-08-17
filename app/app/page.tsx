1e6c874a29e33925c6e029dd91374d418a218fb3
kiv1n <aloha@kiv1n.ru>
2024-08-17 15:16:03 +0300

    Implement landing page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,35 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
-    <main className="container flex flex-col items-center py-10 xl:px-80"></main>
+    <main className="container flex flex-col items-center py-10">
+      <h1 className="text-4xl md:text-5xl font-extrabold tracking-tighter text-center">
+        Sell / Buy NFTs and don&apos;t worry about chains
+      </h1>
+      <h2 className="text-2xl font-normal tracking-tight text-center text-muted-foreground mt-4">
+        A multichain marketplace powered by Wormhole
+      </h2>
+      <div className="flex flex-row gap-2 mt-6">
+        <Link href="/explore">
+          <Button variant="default">👀 Explore</Button>
+        </Link>
+        <Link href="/account">
+          <Button variant="secondary">💸 Sell</Button>
+        </Link>
+      </div>
+      <div className="flex flex-col items-center max-w-[480px] mt-8">
+        <Image
+          src="/images/octopus.png"
+          alt="Octopus"
+          priority={false}
+          width="100"
+          height="100"
+          sizes="100vw"
+          className="w-full"
+        />
+      </div>
+    </main>
   );
 }
