4fdd6b922439648967c3dd026fe28956d868433c
kiv1n <aloha@kiv1n.ru>
2024-08-18 18:05:31 +0300

    Define configs for base sepolia in the app, redeploy some contracts

--- app/config/chains.ts
+++ app/config/chains.ts
@@ -1,8 +1,9 @@
 import { Address } from "viem";
-import { Chain, optimismSepolia } from "viem/chains";
+import { baseSepolia, Chain, optimismSepolia } from "viem/chains";
 
 export type ChainConfig = {
   chain: Chain;
+  wormholeChainId: number;
   market: Address;
 };
 
@@ -11,6 +12,12 @@ export type ChainConfigs = typeof chainConfigs;
 export const chainConfigs = {
   optimismSepolia: {
     chain: optimismSepolia,
+    wormholeChainId: 10005,
     market: "0xeCD9adC284498727f7576267C3C6DC0a14481b1e",
+  } as ChainConfig,
+  baseSepolia: {
+    chain: baseSepolia,
+    wormholeChainId: 10004,
+    market: "0x0c44cFecaFE4da904Ee24984FD74c91C2bE431B7",
   } as ChainConfig,
 };
